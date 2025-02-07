7c225255b0b905e0d25c8dd364f6c7f375dae1f7
MVXIMokda <maks016pro@gmail.com>
2025-02-07 20:40:46 +0300

    ...

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,27 @@ import (
 	"github.com/MVXIMokda/todolist/pkg/handler"
 	"github.com/MVXIMokda/todolist/pkg/repository"
 	"github.com/MVXIMokda/todolist/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	r := repository.NewRepository()
 	services := service.NewService(r)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo_app.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
 
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
